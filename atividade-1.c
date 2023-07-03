6a279518426ca21233d424b1a51761e65d7eb22a
Lorraine Gomes <101226446+Alerpina@users.noreply.github.com>
2023-07-03 00:13:39 -0300

    Update atividade-1.c

--- atividade-1.c
+++ atividade-1.c
@@ -5,7 +5,7 @@ int numero;
 int soma = 3;
 
 int main (){
-    printf ("Olá, seja bem-vindo, digite um número para soma.\n");
+    printf ("Olá, seja bem-vindo, digite um número para a soma.\n");
     scanf ("%d", &numero);
     printf ("Seu número foi %d\n", numero + soma);
 }
