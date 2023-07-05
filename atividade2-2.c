ec44c364790fbe86abf94acf644647a22c792206
Lorraine Gomes <101226446+Alerpina@users.noreply.github.com>
2023-07-05 13:18:00 -0400

    Update and rename atividade-5.c to atividade2-2.c

---
+++ atividade2-2.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+
+int numeromaior;
+int numeromenor;
+
+int main()
+{
+    printf ("Bem-vindo, digite dois números\n");
+    scanf ("%d%d", &numeromenor, &numeromaior);
+
+    if(numeromaior > numeromenor)
+    printf ("%d\n", numeromaior );
+    
+    else (numeromenor < numeromaior);
+    {
+       
+    }
+
+    return 0;
+}
